439dc588bfb1a93803b7c3ebf10156f517e64c47
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-03-12 00:07:39 +0200

    Implemented validate email and password

--- hospital_frontend/script.js
+++ hospital_frontend/script.js
@@ -31,5 +31,60 @@ workshop_pages.loadFor = (page) => {
 }
 
 workshop_pages.load_signup = () => {
-    alert(x);
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let dob = document.getElementById('dob');
+    let position = document.getElementById('position');
+    let submit = document.getElementById('submit');
+
+    function validateEmail() 
+{
+    email_value = email.value;
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email_value))
+    {
+        document.getElementById("emailmessage").innerText = "";
+        return (true)
+        
+    }
+    document.getElementById("emailmessage").innerText = "You have entered an invalid email address!";
+    
+    return (false)
+}
+
+
+
+function validatePassword() {
+    password_value = password.value;
+        errors = [];
+    if (password_value.length < 8) {
+        errors.push("Your password must be at least 8 characters..."); 
+    }
+    if (password_value.search(/[A-Z]/) < 0) {
+        errors.push("Your password must contain at least one capital letter...");
+    }
+    if (password_value.search(/[0-9]/) < 0) {
+        errors.push("Your password must contain at least one digit..."); 
+    }
+    if(password_value.search(/[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]/) < 0){
+        errors.push("Your password must contain at least one special character..."); 
+    }
+    if (errors.length > 0) {
+        let errors_string = errors.join("\n");
+
+        console.log(typeof(errors));
+        document.getElementById("passwordmessage").innerHTML = errors_string;
+        return false;
+    }
+    document.getElementById("passwordmessage").innerHTML = "";
+    return true;
+}
+
+submit.addEventListener("click", function(){
+    
+    validateEmail();
+    validatePassword();
+})
+
+    const post_users_signup = workshop_pages.base_url + "signup.php";
 }
