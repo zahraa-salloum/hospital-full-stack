640f716818f3a332311ccd35e4d70c61d88360b1
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-03-11 23:12:57 +0200

    Implemented the clean JS file

--- hospital_frontend/script.js
+++ hospital_frontend/script.js
@@ -2,3 +2,34 @@ const workshop_pages = {};
 
 workshop_pages.base_url = "http://localhost/hospital-full-stack/hospital_backend";
 
+workshop_pages.getAPI = async (api_url) => {
+    try{
+        return await axios(api_url);
+    }catch(error){
+        console.log("Error from GET API");
+    }
+}
+
+workshop_pages.postAPI = async (api_url, api_data, api_token = null) => {
+    try{
+        return await axios.post(
+            api_url,
+            api_data,
+            {
+                headers:{
+                    'Authorization' : "token " + api_token
+                }
+            }
+        );
+    }catch(error){
+        console.log("Error from POST API");
+    }
+}
+
+workshop_pages.loadFor = (page) => {
+    eval("workshop_pages.load_" + page + "();");
+}
+
+workshop_pages.load_signup = () => {
+    alert(x);
+}
