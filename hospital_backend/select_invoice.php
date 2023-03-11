144181153b30c05cfb8d5690a0e0e28fa3ec98fa
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-03-11 15:38:00 +0200

    Created select invoice

---
+++ hospital_backend/select_invoice.php
@@ -0,0 +1,38 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+include('connection.php');
+
+$id = $_POST['id'];
+
+$response['status'] = "Something went wrong";
+
+
+
+$query = $mysqli->prepare('select * from invoices where id =?');
+$query->bind_param('s', $id);
+$query->execute();
+
+$query->store_result();
+$num_rows = $query->num_rows();
+$query->bind_result($id, $user_id, $hospital_id, $total_amount,$date_issued);
+$query->fetch();
+
+
+$response = [];
+if ($num_rows == 0) {
+    $response['response'] = "invoice not found";
+} else {
+    
+        
+        $response['id'] = $id;
+        $response['user_id'] = $user_id;
+        $response['hospital_id'] = $hospital_id;
+        $response['total_amount'] = $total_amount;
+        $response['date_issued'] = $date_issued;
+
+    }
+
+
+
+echo json_encode($response);
