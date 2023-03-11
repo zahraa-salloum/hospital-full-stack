512f31e1806a198553944de4935e41d8e67f0afe
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-03-11 03:04:48 +0200

    Created the select beds

---
+++ hospital_backend/select_beds.php
@@ -0,0 +1,36 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+include('connection.php');
+
+$department_id = $_POST['department_id'];
+$hospital_id = $_POST['hospital_id'];
+$room_id = $_POST['room_id'];
+
+$response['status'] = "Something went wrong";
+
+$query = $mysqli->prepare('select * from beds where department_id = ? AND hospital_id = ? AND room_id = ? AND taken = 0');
+$query->bind_param('iii', $department_id,$hospital_id,$room_id);
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows();
+$query->bind_result($id,$room_id, $hospital_id, $department_id,$taken);
+$response = [];
+
+// echo $query;
+if ($num_rows == 0) {
+    $response['response'] = "no beds";
+} else {
+    while ($query->fetch() && $num_rows !== 0)  {
+        $data = array(
+            'id' => $id,
+            'room_id' => $room_id,
+            'hospital_id' => $hospital_id,
+            'department_id' => $department_id,
+            'taken' => $taken
+            );
+            array_push($response, $data);
+}
+}
+
+echo json_encode($response);
