9d7020e1d38538971cd02bc16d530c44a579205d
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-03-11 03:57:53 +0200

    Created choose department for user

---
+++ hospital_backend/insert_user_department.php
@@ -0,0 +1,19 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+include('connection.php');
+
+$department_id = $_POST['department_id'];
+$hospital_id = $_POST['hospital_id'];
+$user_id = $_POST['user_id'];
+
+$response['status'] = "Something went wrong";
+
+if($user_id != null){
+    $query = $mysqli->prepare('insert into user_departments (user_id, department_id, hospital_id) values (?, ?, ?)');
+    $query->bind_param('iii', $user_id, $department_id, $hospital_id);
+    $query->execute();
+    $response['status'] = "success";
+}
+
+echo json_encode($response);
