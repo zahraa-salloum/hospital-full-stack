9a3d973610da64df6280046d9573d1e03ea267ce
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-03-12 15:15:03 +0200

    Created select employees php

---
+++ hospital_backend/select_employees.php
@@ -0,0 +1,28 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+include('connection.php');
+
+$response['status'] = "Something went wrong";
+
+$query = $mysqli->prepare('select id,name from users where usertype_id = 2');
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows();
+$query->bind_result($id,$name);
+$response = [];
+
+// echo $query;
+if ($num_rows == 0) {
+    $response['response'] = "no employees";
+} else {
+    while ($query->fetch() && $num_rows !== 0)  {
+        $data = array(
+            'id' => $id,
+            'name' => $name
+            );
+            array_push($response, $data);
+}
+}
+
+echo json_encode($response);
